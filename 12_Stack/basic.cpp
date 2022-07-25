e4f669911c27174299a910b0ee9c2a64e72a89bd
anujmehere <mehereanuj570@gmail.com>
2022-07-25 23:46:09 +0530

    stack

--- 12_Stack/basic.cpp
+++ 12_Stack/basic.cpp
@@ -38,11 +38,93 @@ struct Stack
     }
 };
 
-//Now Lets try to
+//Now Lets try to generate stack using vectors
+struct VecStack
+{
+    vector<int> v;
+    void push(int x)
+    {
+        v.push_back(x);
+    }
+    int pop()
+    {
+        int res=v.back();
+        v.pop_back();
+        return res;
+    }
+    int peek()
+    {
+        return v.back();
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool isEmpty()
+    {
+        return v.empty();
+    }
+};
+
+//Linked List Implementation of Stack 
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int d)
+    {
+        data=d;
+        next=NULL;
+    }
+};
+struct LLStack
+{
+    Node *head;
+    int sz;
+    LLStack()
+    {
+        head=NULL;
+        sz=0;
+    }
+    void push(int x)
+    {
+        Node *temp=new Node(x);
+        temp->next=head;
+        head=temp;
+        sz++;
+    }
+    int pop()
+    {
+        if(head==NULL)
+            return INT_MAX;
+        int res=head->data;
+        Node *temp=head;
+        head=head->next;
+        delete temp;
+        sz--;
+        return res;
+    }
+    int peek()
+    {
+        if(head==NULL)
+            return INT_MAX;
+        return head->data;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool isEmpty()
+    {
+        return (head==NULL);
+    }
+
+};
 
 int main()
 {
-    Stack s(5);
+    LLStack s;
+    //VecStack s;
     s.push(50);
     s.push(40);
     s.push(30);
